c56241cfb98b3c040ceae654e9715d845bcce76b
emhaki <mhmh779@naver.com>
2023-07-18 22:31:51 +0900

    Error Handler 추가

--- sns/src/main/java/com/fastcampus/sns/model/entity/UserEntity.java
+++ sns/src/main/java/com/fastcampus/sns/model/entity/UserEntity.java
@@ -1,14 +1,25 @@
 package com.fastcampus.sns.model.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import com.fastcampus.sns.model.UserRole;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
+
+import java.sql.Timestamp;
+import java.time.Instant;
 
 @Entity
+@Table(name = "\"user\"")
+@Getter
+@Setter
+@SQLDelete(sql = "UPDATE \"user\" SET deleted_at = NOW() WHERE id=?")
+@Where(clause = "deleted_at is NULL")
 public class UserEntity {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
     @Column(name="user_name")
@@ -16,4 +27,32 @@ public class UserEntity {
 
     @Column(name="password")
     private String password;
+
+    @Column(name = "role")
+    @Enumerated(EnumType.STRING)
+    private UserRole role = UserRole.USER;
+
+    @Column(name = "registered_at")
+    private Timestamp registeredAt;
+
+    @Column(name = "updated_at")
+    private Timestamp updatedAt;
+
+    @Column(name = "deleted_at")
+    private Timestamp deletedAt;
+
+    @PrePersist
+    void registeredAt() {
+        this.registeredAt = Timestamp.from(Instant.now());
+    }
+    @PreUpdate
+    void updatedAt() {
+        this.updatedAt = Timestamp.from(Instant.now());
+    }
+
+    public static UserEntity of(String userName, String password) {
+        UserEntity userEntity = new UserEntity();
+        userEntity.setUserName(userName);
+        return userEntity;
+    }
 }
