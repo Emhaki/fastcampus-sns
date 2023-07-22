9e34cbd4224f4d1899b5226fb3beeb1794e91aca
emhaki <mhmh779@naver.com>
2023-07-22 14:17:24 +0900

    Jwt Token 생성

--- sns/src/main/java/com/fastcampus/sns/service/UserService.java
+++ sns/src/main/java/com/fastcampus/sns/service/UserService.java
@@ -5,9 +5,12 @@ import com.fastcampus.sns.exception.SnsApplicationException;
 import com.fastcampus.sns.model.User;
 import com.fastcampus.sns.model.entity.UserEntity;
 import com.fastcampus.sns.repository.UserEntityRepository;
+import com.fastcampus.sns.util.JwtTokenUtils;
 import lombok.RequiredArgsConstructor;
+import lombok.Value;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
@@ -18,6 +21,13 @@ public class UserService {
     private final UserEntityRepository userEntityRepository;
     private final BCryptPasswordEncoder encoder;
 
+    @Value("${jwt.secret.key}")
+    private String secretKey;
+
+    @Value("${jwt.token.expired-time-ms}")
+    private Long expiredTimeMs;
+
+    @Transactional
     public User join(String userName, String password) {
         // 회원가입하는 userName으로 회원가입된 user가 있는지
         userEntityRepository.findByUserName(userName).ifPresent(it -> {
@@ -32,15 +42,17 @@ public class UserService {
 
     public String login(String userName, String password) {
         // 회원가입 여부 체크
-        UserEntity userEntity = userEntityRepository.findByUserName(userName).orElseThrow(() -> new SnsApplicationException(ErrorCode.DUPLICATED_USER_NAME, ""));
+        UserEntity userEntity = userEntityRepository.findByUserName(userName).orElseThrow(() -> new SnsApplicationException(ErrorCode.USER_NOT_FOUND, String.format("%s not founded", userName)));
 
         // 비밀번호 체크
-        if(!userEntity.getPassword().equals(password)) {
-            throw  new SnsApplicationException(ErrorCode.DUPLICATED_USER_NAME, "");
+        if(encoder.matches(password, userEntity.getPassword())){
+//        if(!userEntity.getPassword().equals(password)) {
+            throw  new SnsApplicationException(ErrorCode.INVALID_PASSWORD, "%s is invalid");
         }
         // 토큰 생성
+        String token = JwtTokenUtils.generateToken(userName, secretKey, expiredTimeMs);
 
-        return "";
+        return token;
     }
 
     public String login() {
