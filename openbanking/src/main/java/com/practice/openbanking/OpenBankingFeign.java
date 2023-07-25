de010167070e20b266a7f3aa2708b4cbd6e84637
emhaki <mhmh779@naver.com>
2023-07-25 22:37:02 +0900

    authorize 완성

---
+++ openbanking/src/main/java/com/practice/openbanking/OpenBankingFeign.java
@@ -0,0 +1,19 @@
+package com.practice.openbanking;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+//@FeignClient(name="feign", url="https://testapi.openbanking.or.kr")
+//public interface OpenBankingFeign {
+//    @PostMapping(
+//            path = "/oauth/2.0/token",
+//            consumes = "application/x-www-form-urlencoded",
+//            produces = "application/json")
+//    public TokenResponseDto requestToken(
+//            @RequestParam("code") String code,
+//            @RequestParam("client_id") String client_id,
+//            @RequestParam("client_secret") String client_secret,
+//            @RequestParam("redirect_uri") String redirect_uri,
+//            @RequestParam("grant_type") String grant_type);
+//}
--- openbanking/src/main/java/com/practice/openbanking/controller/CommonController.java
+++ openbanking/src/main/java/com/practice/openbanking/controller/CommonController.java
@@ -1,5 +1,6 @@
 package com.practice.openbanking.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,10 +8,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class CommonController {
 
+    @Value("${openbanking.client-id}")
+    private String clientId;
+
+    @Value("${openbanking.client-Secret}")
+    private String clientSecret;
+    private String redirectUrl = "http://localhost:8080/auth/openbank/callback";
+
     @RequestMapping("/main")
     public String main(Model model) {
 
-        model.addAttribute();
+        model.addAttribute("redirectUrl", redirectUrl);
+        model.addAttribute("clientId", clientId);
         return "main";
     }
 }
