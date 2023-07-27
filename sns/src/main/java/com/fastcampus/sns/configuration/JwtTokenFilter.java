a35a89db4771537c195110d4484eae4c3e677688
emhaki <mhmh779@naver.com>
2023-07-27 22:25:41 +0900

    Create Jwt

---
+++ sns/src/main/java/com/fastcampus/sns/configuration/JwtTokenFilter.java
@@ -0,0 +1,72 @@
+package com.fastcampus.sns.configuration;
+
+
+import com.fastcampus.sns.model.User;
+import com.fastcampus.sns.service.UserService;
+import com.fastcampus.sns.util.JwtTokenUtils;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.List;
+
+@Slf4j
+@RequiredArgsConstructor
+public class JwtTokenFilter extends OncePerRequestFilter {
+
+    private final String key;
+    private final UserService userService;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+        //get header
+        final String header = request.getHeader(HttpHeaders.AUTHORIZATION);
+        if (header == null || !header.startsWith("Bearer ")) {
+            logger.error("Error occurs while getting header");
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        try {
+            final String token = header.split("")[1].trim();
+
+            // TODO : check token is valid
+            if(JwtTokenUtils.isExpired(token, key)) {
+                log.error("Key is expired");
+                filterChain.doFilter(request, response);
+                return;
+            }
+
+            // get username from token
+            String userName = JwtTokenUtils.getUserName(token, key);
+            // check the user is valid
+
+            User user = userService.loadUserByUserName(userName);
+
+            UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
+
+                    user, null, List.of(new SimpleGrantedAuthority(user.getUserRole().toString()))
+            );
+            authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+
+            SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+        } catch (RuntimeException e) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        filterChain.doFilter(request, response);
+    }
+
+}
--- sns/src/main/java/com/fastcampus/sns/configuration/SecurityConfig.java
+++ sns/src/main/java/com/fastcampus/sns/configuration/SecurityConfig.java
@@ -1,18 +1,28 @@
 package com.fastcampus.sns.configuration;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
 
     @Bean
     public BCryptPasswordEncoder encodePassword() {
         return new BCryptPasswordEncoder();
     }
+
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -21,5 +31,21 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
 
         return http.build();
+    }
+
+    // https://velog.io/@devmin/Spring-Security-Configuration
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowCredentials(false); // 쿠키를 받을건지
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:8080"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST"));
+
+        configuration.addAllowedHeader("*");
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+
     }
 }
